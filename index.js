fda003d7e2166c7a9495e46956f86f7abff0f5b2
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-07-17 22:30:48 +0300

    Calculate income over time

--- index.js
+++ index.js
@@ -4,25 +4,42 @@ const shedB=document.getElementById("shedB")
 const shedC=document.getElementById("shedC")
 const shedD=document.getElementById("shedD")
 const output=document.querySelector(".output")
+const price=45
+
 
 const submit=document.getElementById("button")
 function incomeOverTime(sellingPrice,time,litres){
     return sellingPrice*time*litres
 }
 submit.addEventListener("click",function(){
-    let total=shedA.value + shedB.value + shedC.value + shedD.value
+    let total=parseInt (shedA.value) + parseInt (shedB.value) + parseInt (shedC.value) + parseInt (shedD.value)
 
     let pa=document.createElement("p")
-    pa.innerHTML=`Your production in Shed A is ${shedA} litres per day`
+    pa.innerHTML=`Your production in Shed A is ${shedA.value} litres per day`
     
     let pb=document.createElement("p")
-    pb.innerHTML=`Your production in Shed B is ${shedB} litres per day`
+    pb.innerHTML=`Your production in Shed B is ${shedB.value} litres per day`
 
     let pc=document.createElement("p")
-    pc.innerHTML=`Your production in Shed C is ${shedC} litres per day`
+    pc.innerHTML=`Your production in Shed C is ${shedC.value} litres per day`
 
     let pd=document.createElement("p")
-    pd.innerHTML=`Your production in Shed D is ${shedD} litres per day`
+    pd.innerHTML=`Your production in Shed D is ${shedD.value} litres per day`
+    
+    output.appendChild(pa)
+    output.appendChild(pb)
+    output.appendChild(pc)
+    output.appendChild(pd)
+
+    let pw=document.createElement("p")
+    pw.innerHTML=`Your weekly income is ${incomeOverTime(price,7,total)} Kshs`
+
+    output.appendChild(pw)
+
+    let py=document.createElement("p")
+    py.innerHTML=`Your yearly income is ${incomeOverTime(price,366,total)} Kshs`
+
+    output.appendChild(py)
     
 
 
