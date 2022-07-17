84d8e6914675c377f4188fc5d92993e57ce524ab
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-07-17 19:52:50 +0300

    Change font of class fonts

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // alert ()
+const shedA=document.getElementById("ShedA").value
+const shedB=document.getElementById("ShedB").value
+const shedC=document.getElementById("ShedC").value
+const shedD=document.getElementById("ShedD").value
+
+const submit=document.getElementById("")
